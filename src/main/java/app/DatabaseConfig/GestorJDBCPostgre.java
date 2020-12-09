0c11310135749c58eaeaccb9e3d029d757d0138e
Dash166 <joseluis_1555@hotmail.com>
2020-12-09 17:02:25 -0500

    update version 1.0

--- src/main/java/app/DatabaseConfig/GestorJDBCPostgre.java
+++ src/main/java/app/DatabaseConfig/GestorJDBCPostgre.java
@@ -15,11 +15,11 @@ public class GestorJDBCPostgre extends GestorJDBC{
      @Override
      public void abrirConexion() throws Exception {
         try {
-            String host="ec2-3-95-87-221.compute-1.amazonaws.com";
-            String user="iadtrawmfvjzwh";
-            String password="c643fe00c328bf0948751f44e7f8b40e15fc05d7af256e7911553f37341c79af";
+            String host="ec2-3-208-50-226.compute-1.amazonaws.com";
+            String user="puhpdvsrudzyxt";
+            String password="3c490415e62f6f9239844a9e45d070ea27da3d9669f04e37426bcc4a6b379c07";
             String port="5432";
-            String database="dam792urv1f2kp";
+            String database="ddl6bjifq5a25k";
             String url = "jdbc:postgresql://"+host+":"+port+"/"+database+"?sslmode=require&&?targetServerType=primary";
              Class.forName("org.postgresql.Driver");
             conexion = DriverManager.getConnection(url,user,password);
